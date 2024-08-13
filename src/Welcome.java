171a6e79482200dd615c7a3c2f7dc5a7acfdb181
KhangTann <23722301.khang@student.iuh.edu.vn>
2024-08-13 20:11:34 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Tan Khang!");
+        System.out.println("Welcome to the DHKTPM19B!");
+    }
+}
